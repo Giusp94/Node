dc962af0e0c17e844f3dbe3a2099a2fb9354d90a
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-10-09 12:52:55 +0200

    Correct ex setup express app, because I don t understand to use dotenv

--- setup/src/server.ts
+++ setup/src/server.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import "express-async-errors";
 import morgan from "morgan";
+// DOTENV si utilizza per tenere in un file separato informazioni
+// quali port, database, crittografia, ecc
+import "dotenv/config";
 
 // Dichiarazione della porta di ascolto e del route
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Dichiarazione dell'utilizzo del tipo json e
 // registrazione delle richieste del Cliente,
@@ -31,7 +34,10 @@ let planets: Planets = [
     name: "Mars",
   },
 ];
-
-app.listen(port, () => {
+app.get("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send(planets);
+});
+app.listen(port, (req: Request, res: Response) => {
   console.log(`App in ascolto su http://localhost:${port}`);
 });
